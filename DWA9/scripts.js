22475c4b3fbc39b555b2fb4c7baa6787ccfe9792
Ndimande <mkatroso.deep@gmail.com>
2023-08-04 09:51:25 +0200

    commit

--- DWA9/scripts.js
+++ DWA9/scripts.js
@@ -1,3 +1,7 @@
+// @ts-check
+/* eslint-disable import/extensions */
+/* eslint-disable no-restricted-syntax */
+
 import { state } from "./modules/state.js";
 import { books, authors, genres } from "./modules/data.js";
 import { THEMES } from "./modules/config.js";
@@ -17,9 +21,10 @@ import {
   handleSearchOpen,
   handleSearchSubmit,
 } from "./modules/search.js";
-import "../DWA9/components/active-preview.js";
+import "./components/active-preview.js";
 import { handleShowMore } from "./modules/showMore.js";
 
+// App setup ----------------------------------------------------------------------------------
 
 // Handles default theme setting preferences
 if (
@@ -44,17 +49,17 @@ createDropdownList(authors, "Authors", html.search.authors);
 createPreviewsList(state.matches, state.page);
 updateShowMoreButton(state.matches, state.page);
 
-// Settings menu operations  
+// Settings menu operations  ------------------------------------------------------------------
 html.headerButtons.settings.addEventListener("click", handleSettingsOpen);
 html.settings.cancel.addEventListener("click", handleSettingsCancel);
 html.settings.form.addEventListener("submit", handleSettingsSubmit);
 
-// Search menu operations 
+// Search menu operations ---------------------------------------------------------------------
 html.headerButtons.search.addEventListener("click", handleSearchOpen);
 html.search.cancel.addEventListener("click", handleSearchCancel);
 html.search.form.addEventListener("submit", handleSearchSubmit);
 
-// Active previews 
+// Active previews ----------------------------------------------------------------------------
 /**
  * Event handler that fires when one of the list preview items in the main list area is
  * clicked and opens an active preview that displays a larger version of the cover and
@@ -66,5 +71,5 @@ const handleOpenActivePreview = (event) => {
 };
 html.list.items.addEventListener("click", handleOpenActivePreview);
 
-// Show more button  
+// Show more button  --------------------------------------------------------------------------
 html.list.button.addEventListener("click", handleShowMore);
